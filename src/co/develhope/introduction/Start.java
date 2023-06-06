240f631e4e2234d6737859a5d1b145154c5f3a74
uroNana <mirianamartinelli97@libero.it>
2023-06-06 23:46:35 +0200

    output hello student + birthday

---
+++ src/co/develhope/introduction/Start.java
@@ -0,0 +1,8 @@
+package co.develhope.introduction;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello develhope java student " + "February 6, 1997");
+    }
+
+}
